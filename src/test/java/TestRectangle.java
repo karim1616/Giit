edcdc86d9e7337670f09949be08b3fbda1df093c
unknown <karim.younes@aol.com>
2020-01-17 13:00:58 +0100

    Initial commit

---
+++ src/test/java/TestRectangle.java
@@ -0,0 +1,8 @@
+public class TestRectangle {
+    public static void main(String[] args) {
+
+        Rectangle monRectangle = new Rectangle(4.0, 5.0);
+        System.out.println("Test OK");
+        monRectangle.afficher();
+    }
+}
